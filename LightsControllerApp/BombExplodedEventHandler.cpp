5febb6af47bcbf35082c40b75616154ba1eacb64
gabirat <gabirat@onet.pl>
2021-06-22 20:24:04 +0200

    added bobmexpodedevent for future implementation

---
+++ LightsControllerApp/BombExplodedEventHandler.cpp
@@ -0,0 +1,5 @@
+#include "BombExplodedEventHandler.h"
+
+namespace GameEvent {
+
+};
---
+++ LightsControllerApp/BombExplodedEventHandler.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "EventHandler.h"
+
+namespace GameEvent {
+
+class BombExplodedEventHandler : public EventHandler {
+};
+
+};
+
+
+

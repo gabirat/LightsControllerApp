9dad0b912b96a19a5d6c21c924bc0742061c9277
gabirat <gabirat@onet.pl>
2021-06-22 22:11:01 +0200

    added threads for event loop and queue handling

--- LightsControllerApp/GameHandler.cpp
+++ LightsControllerApp/GameHandler.cpp
@@ -1,26 +1,59 @@
 #include "GameHandler.h"
 
-GameHandler::GameHandler(std::shared_ptr<HardwareController> hc, std::string gsi_path, Poco::UInt16 port) : 
+void GameHandler::check_events() {
+	while (running) {
+		data_lock.lock();
+		if (data.has("previously") &&
+			data.getObject("previously")->has("round") &&
+			data.getObject("previously")->getObject("round")->has("bomb") &&
+			(data.getObject("previously")->getObject("round")->getValue<std::string>("bomb") == "planted")) {
+			if (data.has("round") &&
+				data.getObject("round")->has("bomb") &&
+				(data.getObject("round")->getValue<std::string>("bomb") == "exploded")) {
+				queue_lock.lock();
+				event_queue.push(GameEvent::EventHandlerFactory::create_event_handler(GameEvent::EventType::BOMB_EXPLODED, led_controller));
+				queue_lock.unlock();
+			}
+		}
+		data_lock.unlock();
+	}
+}
+
+void GameHandler::handle_queue() {
+	while (running) {
+		queue_lock.lock();
+		if (event_queue.empty()) {
+			continue;
+		}
+		auto& ev = event_queue.front();
+		ev->handle_event();
+		event_queue.pop();
+		queue_lock.unlock();
+		Sleep(16);
+	}
+}
+
+GameHandler::GameHandler(std::shared_ptr<HardwareController> hc, std::string gsi_path, Poco::UInt16 port) :
 	led_controller(hc), 
 	gsi_path(gsi_path),
-	port(port) {
+	port(port),
+	running(false) {
 	http_srv = std::make_shared<Poco::Net::HTTPServer>(
 		new GSIRequestHandlerFactory(this),
 		Poco::Net::ServerSocket(port),
 		new Poco::Net::HTTPServerParams);
-
 }
 
 void GameHandler::update_data(Poco::JSON::Object::Ptr data) {
-	data_mutex.lock();
+	data_lock.lock();
 	this->data = *data;
-	data_mutex.unlock();
+	data_lock.unlock();
 }
 
 Poco::JSON::Object GameHandler::get_data() {
-	data_mutex.lock();
+	data_lock.lock();
 	auto ret = data;
-	data_mutex.unlock();
+	data_lock.unlock();
 	return ret;
 }
 
@@ -29,9 +62,15 @@ std::string GameHandler::get_gsi_path() const {
 }
 
 void GameHandler::run() {
+	running = true;
 	http_srv->start();
+	event_loop_thread = std::thread(&GameHandler::check_events, this);
+	queue_handler_thread = std::thread(&GameHandler::handle_queue, this);
 }
 
 void GameHandler::stop() {
+	running = false;
 	http_srv->stop();
+	event_loop_thread.join();
+	queue_handler_thread.join();
 }
--- LightsControllerApp/GameHandler.h
+++ LightsControllerApp/GameHandler.h
@@ -1,23 +1,30 @@
 #pragma once
 
 #include <vector>
+#include <queue>
 #include <memory>
 #include <thread>
 #include <mutex>
 #include <Poco/Net/HTTPServer.h>
 #include "HardwareController.h"
 #include "GSIRequestHandlerFactory.h"
-#include "EventHandler.h"
+#include "EventHandlerFactory.h"
 
 class GameHandler {
 private:
 	std::shared_ptr<Poco::Net::HTTPServer> http_srv;
 	std::shared_ptr<HardwareController> led_controller;
-	std::vector<EventHandler> event_handlers;
+	std::queue<std::shared_ptr<GameEvent::EventHandler>> event_queue;
 	std::string gsi_path;
 	Poco::UInt16 port;
-	std::mutex data_mutex;
+	std::mutex data_lock;
+	std::mutex queue_lock;
 	Poco::JSON::Object data;
+	bool running;
+	std::thread event_loop_thread;
+	std::thread queue_handler_thread;
+	void check_events();
+	void handle_queue();
 
 public:
 	GameHandler(std::shared_ptr<HardwareController> hc, std::string gsi_path, Poco::UInt16 port);

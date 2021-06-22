a9f908ae29d326d1a563d20a424acc03c5756a59
gabirat <gabirat@onet.pl>
2021-06-22 22:10:04 +0200

    added generic event handler

--- LightsControllerApp/EventHandler.cpp
+++ LightsControllerApp/EventHandler.cpp
@@ -1,4 +1,14 @@
 #include "EventHandler.h"
 
 namespace GameEvent {
+    EventHandler::EventHandler(std::shared_ptr<HardwareController> hc) : event_type(EventType::None), led_controller(hc) {
+    }
+
+    EventType EventHandler::get_event_type() const {
+        return event_type;
+    }
+
+    void EventHandler::handle_event() {
+        led_controller->play_effect(effect);
+    }
 };

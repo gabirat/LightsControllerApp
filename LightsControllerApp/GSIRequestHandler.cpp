e3ff6a1314602112212e729f4ec565d2d3d35e37
gabirat <gabirat@onet.pl>
2021-06-22 20:25:06 +0200

    fixed connection reset error by sending the status

--- LightsControllerApp/GSIRequestHandler.cpp
+++ LightsControllerApp/GSIRequestHandler.cpp
@@ -5,10 +5,9 @@ GSIRequestHandler::GSIRequestHandler(GameHandler* gh) : game_handler(gh) {
 
 void GSIRequestHandler::handleRequest(Poco::Net::HTTPServerRequest& req, Poco::Net::HTTPServerResponse& resp) {
     if (game_handler->get_gsi_path() != req.getURI()) return;
-
     Poco::JSON::Parser parser;
     auto result = parser.parse(req.stream());
-    auto obj = result.extract<Poco::JSON::Object::Ptr>();
-    game_handler->update_data(obj);
+    game_handler->update_data(result.extract<Poco::JSON::Object::Ptr>());
     resp.setStatus(Poco::Net::HTTPResponse::HTTP_OK);
+    resp.send();
 }

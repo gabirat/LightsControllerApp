4df097b3bd67e7c161cfd9bf4b17c99b37ea31e7
gabirat <gabirat@onet.pl>
2021-06-22 13:52:58 +0200

    added constructor

--- LightsControllerApp/CSGOHandler.cpp
+++ LightsControllerApp/CSGOHandler.cpp
@@ -1,1 +1,5 @@
 #include "CSGOHandler.h"
+
+CSGOHandler::CSGOHandler(std::shared_ptr<HardwareController> hc, std::string gsi_path, Poco::UInt16 port) : 
+	GameHandler(hc, gsi_path, port) {
+}
--- LightsControllerApp/CSGOHandler.h
+++ LightsControllerApp/CSGOHandler.h
@@ -1,5 +1,10 @@
 #pragma once
+
 #include "GameHandler.h"
+
 class CSGOHandler : public GameHandler {
+private:
+public:
+	CSGOHandler(std::shared_ptr<HardwareController> hc, std::string gsi_path, Poco::UInt16 port);
 };
 

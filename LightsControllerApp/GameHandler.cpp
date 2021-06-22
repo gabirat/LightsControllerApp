4243edcdbcec5868e0d62ee998a4f72a64cf6b15
gabirat <gabirat@onet.pl>
2021-06-22 12:09:05 +0200

    more config options for GameHandler

--- LightsControllerApp/GameHandler.cpp
+++ LightsControllerApp/GameHandler.cpp
@@ -1,7 +1,14 @@
 #include "GameHandler.h"
 
-GameHandler::GameHandler() {
-	http_srv = std::make_shared<Poco::Net::HTTPServer>(new GSIRequestHandlerFactory(this), Poco::Net::ServerSocket(1337), new Poco::Net::HTTPServerParams);
+GameHandler::GameHandler(std::shared_ptr<HardwareController> hc, std::string gsi_path, Poco::UInt16 port) : 
+	led_controller(hc), 
+	gsi_path(gsi_path),
+	port(port) {
+	http_srv = std::make_shared<Poco::Net::HTTPServer>(
+		new GSIRequestHandlerFactory(this),
+		Poco::Net::ServerSocket(port),
+		new Poco::Net::HTTPServerParams);
+
 }
 
 void GameHandler::update_data(Poco::JSON::Object::Ptr data) {
--- LightsControllerApp/GameHandler.h
+++ LightsControllerApp/GameHandler.h
@@ -5,19 +5,23 @@
 #include <thread>
 #include <mutex>
 #include <Poco/Net/HTTPServer.h>
+#include "HardwareController.h"
 #include "GSIRequestHandlerFactory.h"
 #include "EventHandler.h"
 
 class GameHandler {
 private:
 	std::shared_ptr<Poco::Net::HTTPServer> http_srv;
+	std::shared_ptr<HardwareController> led_controller;
 	std::vector<EventHandler> event_handlers;
+	std::string gsi_path;
+	Poco::UInt16 port;
 	std::mutex data_mutex;
 	Poco::JSON::Object new_data;
 	Poco::JSON::Object previous_data;
 
 public:
-	GameHandler();
+	GameHandler(std::shared_ptr<HardwareController> hc, std::string gsi_path, Poco::UInt16 port);
 	void update_data(Poco::JSON::Object::Ptr data);
 	Poco::JSON::Object get_new_data();
 	Poco::JSON::Object get_previous_data();
